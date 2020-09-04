8416c2b84bd0543571a5810d39f2f931ff0b49d7
gonzalo prampini <gonzaloprampini@gmail.com>
2020-09-04 08:37:00 -0300

    agrego comentarios al ejercicio 1

--- Ejercicios_Expert_SRP/Ejercicio 1/src/Program/Program.cs
+++ Ejercicios_Expert_SRP/Ejercicio 1/src/Program/Program.cs
@@ -19,3 +19,9 @@ namespace Expert_SRP
         }
     }
 }
+
+/*
+La clase Kiosco cumple con el patrón Expert, ya que el método ConvertirPesos es responsable de convertir el dinero en una moneda determinada 
+La clase alfajor cumple el patrón Expert porque es responsable de los datos del alfajor. 
+La clase alfajor no cumple con el patrón SRP porque cada clase no puede tener más de un motivo de cambio. Precio y masa no permite un cambio en el futuro. Si en un futuro, por ejemplo, el precio final es precioDulce + precioMasa + 10%, debería cambiar en todos lados donde fue programada esta funcionalidad. No esta sujeto a una responsabilidad. Para que cumpla con la condición de cambio en el futuro debería tener un método precioFinal en la clase Alfajor. 
+*/
