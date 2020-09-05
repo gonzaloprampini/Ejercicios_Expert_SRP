6b9fa026f00efddcf116861f05bed24ce457a7fe
gonzalo prampini <gonzaloprampini@gmail.com>
2020-09-05 00:45:20 -0300

    creo ejercicio 4 y elimino clase programa y otros

---
+++ Ejercicios_Expert_SRP/Ejercicio 4/src/Program/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Cumple con respecto al Expert porque el responsable de hacer
+las citas es el metodo createAppointment que a su vez
+valida los campos de las citas
+Tambien cumple con el SRP porque no se necesita validar los campos
+de manera individual. hay un metodo que soluciona la validacion de
+los datos, que es el createAppoinment. 
+*/
+
+using System;
+using Library;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string appointmentResult = AppointmentService.CreateAppointment("Steven Jhonson", "986782342", "5555-555-555", DateTime.Now, "Wall Street", "Armand");
+            Console.WriteLine(appointmentResult);
+
+            string appointmentResult2 = AppointmentService.CreateAppointment("Ralf Manson", "", "5555-555-555", DateTime.Now, "Queen Street", "");
+            Console.WriteLine(appointmentResult2);
+        }
+    }
+}
